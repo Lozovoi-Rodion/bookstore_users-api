2c593748df4e664283a583d8b9bbf8713759f2c4
Lozovoi Rodion <rodionlozovoi@gmail.com>
2021-10-27 19:36:09 +0300

    WIP: adding services and domain

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -1,12 +1,30 @@
 package users
 
 import (
+	"github.com/Lozovoi-Rodion/bookstore_users-api/domain/users"
+	"github.com/Lozovoi-Rodion/bookstore_users-api/services"
+	"github.com/Lozovoi-Rodion/bookstore_users-api/utils/errors"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func CreateUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "implement me")
+	var user users.User
+
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.RestErr{
+			"invalid json body",
+			http.StatusBadRequest,
+			"bad_request",
+		}
+		c.JSON(restErr.Status, restErr)
+	}
+
+	result, saveErr := services.CreateUser(user)
+	if saveErr != nil {
+		// TODO: handle user creation error
+	}
+	c.JSON(http.StatusCreated, result)
 }
 
 func GetUser(c *gin.Context) {
