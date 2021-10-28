7a387311f22b4ae3ab5564badbd993ff389a9d91
Lozovoi Rodion <rodionlozovoi@gmail.com>
2021-10-28 15:08:36 +0300

    finished dao layer, added date util

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -6,29 +6,41 @@ import (
 	"github.com/Lozovoi-Rodion/bookstore_users-api/utils/errors"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 )
 
 func CreateUser(c *gin.Context) {
 	var user users.User
 
 	if err := c.ShouldBindJSON(&user); err != nil {
-		restErr := errors.RestErr{
-			"invalid json body",
-			http.StatusBadRequest,
-			"bad_request",
-		}
+		restErr := errors.NewBadRequestError("invalid json body")
 		c.JSON(restErr.Status, restErr)
+		return
 	}
 
 	result, saveErr := services.CreateUser(user)
 	if saveErr != nil {
-		// TODO: handle user creation error
+		c.JSON(saveErr.Status, saveErr)
+		return
 	}
 	c.JSON(http.StatusCreated, result)
 }
 
 func GetUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "implement me")
+	userId, userErr := strconv.ParseInt(c.Param("user_id"), 10, 64)
+	if userErr != nil {
+		restErr := errors.NewBadRequestError("invalid user id")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+
+	result, getErr := services.GetUser(userId)
+	if getErr != nil {
+		c.JSON(getErr.Status, getErr)
+		return
+	}
+
+	c.JSON(http.StatusOK, result)
 }
 
 func SearchUser(c *gin.Context) {
